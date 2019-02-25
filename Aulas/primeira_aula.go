8cd2d2f4990cb772c99dc9dc085674c26936c009
Lucas Almeida <lucas.feijo2@hotmail.com>
2019-02-25 20:43:57 -0300

    hello world Go

---
+++ Aulas/primeira_aula.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+	falar()
+}
+/*
+func teste(){
+	for i:= 0;i<100;i++{
+		if i%2 ==0{
+			fmt.Print(i)
+		}
+	}
+}
+
+*/
+
+func falar(){
+	fmt.Print("Teste")
+}
