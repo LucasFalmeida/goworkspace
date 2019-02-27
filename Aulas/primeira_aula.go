68d9d123823892b28c4075d60fe0d42860b82d18
Lucas Almeida <lucas.feijo2@hotmail.com>
2019-02-27 03:34:50 -0300

    testes de metodos e funcoes, também polimorfismo basico em GO

--- Aulas/primeira_aula.go
+++ Aulas/primeira_aula.go
@@ -2,16 +2,37 @@ package main
 
 import "fmt"
 
+type doce struct {
+	nome  string
+	valor int
+}
+
+func chocolate(nome string, valor int) {
+	nome_chocolate := nome
+	valor_chocolate := valor
+	doce := doce{
+		nome_chocolate,
+		valor_chocolate,
+	}
+
+	fmt.Println("O primeiro produto é:", doce)
+	fmt.Println("O nome do chocolate é:", nome_chocolate, "e o preço é:", valor_chocolate)
+
+}
+
+func (b doce) falar() {
+	fmt.Println("Eu sou o chocolate!, ", b.nome)
+
+}
+
 func main() {
-var x [5] int
-x[0] = 1
-x[1] = 2
-x[2] = 3
-x[3] = 4
-x[4] = 5
+	fmt.Println("Testando o poder da linguagem GO")
 
-for i,v := range x{
-	fmt.Println(i,v)
+	chocolate("Cacau", 55)
+	//instancia de um novo struc
+	teste := doce{
+		"amendoas", 40,
+	}
+	//chamei o metodo falar pois a variavel teste é do tipo doce, logo é permitido
+	teste.falar()
 }
-}
-
